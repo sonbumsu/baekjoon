a7890b0ee47432c55dedf189c995d43e7cc2fbca
sonbumsu <sonbumsu0529@gmail.com>
2026-03-25 08:03:25 +0900

    [Bronze V] Title: 공통교육과정, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.py
@@ -0,0 +1,12 @@
+course_id = input()
+
+category = course_id[0]
+
+if category == 'F':
+    print("Foundation")
+elif category == 'C':
+    print("Claves")
+elif category == 'V':
+    print("Veritas")
+elif category == 'E':
+    print("Exploration")
